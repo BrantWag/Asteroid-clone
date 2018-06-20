ebbfde3752a2fbc673ea38e9aee14927083f7ca1
Brant Wagner <brawagne@uat.edu>
2018-06-19 22:24:27 -0700

    Add files via upload

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+	public Transform tf;
+    public float speed;
+    public float time;
+
+    // Use this for initialization
+    void Start () {
+		tf = GetComponent<Transform> ();
+        
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Destroy(this.gameObject, time);
+    }
+
+    //Tells bullet to move 
+    public void Move () {
+		tf.position += tf.up * speed* 0.05f;
+	}
+}
